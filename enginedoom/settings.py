9979d718d735b62d68d8135d320a94d67e5648ce
Joabe Nonato <joabe.nonato@tecnogroup.com.br>
2022-11-07 17:13:19 -0300

    Adicionando engine Doom

---
+++ enginedoom/settings.py
@@ -0,0 +1,19 @@
+import math
+
+# game settings
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 0
+
+#Player
+PLAYER_POS = 1.5, 5 #mini_map
+PLAYER_ANGLE= 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
+
+#Ray casting
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
