fd225261daabf2ed9161799264150fdf03829996
joabe <joabe07@gmail.com>
2022-11-18 03:12:59 -0300

    placar

--- fight_engine/placar.py
+++ fight_engine/placar.py
@@ -1,4 +1,3 @@
-from tkinter import Y
 import pygame as pg
 from settings import *
 
@@ -7,6 +6,9 @@ class Placar:
         self.game = game
         self.reverter = 0
         self.velocidade = 1
+        self.barra_largura_padrao = BARRA_ENERGIA
+        self.barra_altura = 40        
+        self.barra_y = 40
 
     def atualizar(self):
        
@@ -16,49 +18,48 @@ class Placar:
         elif self.game.Tempo > 0 and self.game.Player01.saude > 0 and self.game.Player02.saude > 0:
             self.game.Tempo -= 0.035
 
-        # TESTE TESTE TESTE        
+        # # TESTE TESTE TESTE        
         if self.game.Tempo > 0 and self.game.Player01.saude > 0 and self.game.Player02.saude > 0:
             self.reverter += int(self.velocidade)
             self.game.Player01.saude -= int(self.velocidade)
             self.game.Player02.saude -= int(self.velocidade)
 
+    def alinhar_centro(self, largura, destino):
+        return ((destino - largura) / 2)
+
+    def barra_energia(self, esquerda, saude):
+                
+        margem_lateral = 5
+               
+        branca_largura = (TELA_CENTRO_V - (margem_lateral * 2))        
+        
+        x = TELA_CENTRO_V + margem_lateral + 1
+        
+        if esquerda:
+            x = self.alinhar_centro(branca_largura, TELA_CENTRO_V) 
+        
+        bar = pg.Rect(x, self.barra_y, branca_largura, self.barra_altura)
+        pg.draw.rect(self.game.Tela, BRANCO, bar, 3) 
+
+        if esquerda:            
+            pg.draw.rect(self.game.Tela, AMARELO, (x, bar.y + 3, branca_largura , bar.h - 6))            
+            pg.draw.rect(self.game.Tela, VERMELHO, (x, bar.y + 3, self.reverter , bar.h - 6)) 
+        else:            
+            pg.draw.rect(self.game.Tela, AMARELO, (x, bar.y + 3, branca_largura , bar.h - 6)) 
+            pg.draw.rect(self.game.Tela, VERMELHO, (x, bar.y + 3, saude , bar.h - 6))
+        
+
     def desenhar(self):
 
-        def alinhar_centro(largura):
-            return ((TELA_LARGURA - largura) / 2)
-
-        fonte_tempo = pg.font.SysFont('arial', 53, True, False)
-        barra_largura_padrao = BARRA_ENERGIA
-        barra_altura = 40        
-        barra_y = 40
-        margem_lateral = 5
-        barra_x = (margem_lateral + 2)
+        self.barra_energia(True, self.game.Player01.saude)
+        self.barra_energia(False, self.game.Player02.saude)
         
         cor_tempo = AZUL      
         
-        barra02 = pg.Rect(63 , barra_y, barra_largura_padrao, barra_altura)        
-        # energia02 = pg.Rect(barra_tempo.x + 63 , barra_y, self.game.Player02.saude, barra_altura)
-        
-## BACKGROUND BARRA        
-        branca_largura = (TELA_LARGURA - (margem_lateral * 2))        
-        branca = pg.Rect(alinhar_centro(branca_largura), barra_y, branca_largura , barra_altura)   
-        pg.draw.rect(self.game.Tela, AMARELO, (branca)) 
-        
-# #barra esquerda Player 01
-        barra01 = pg.Rect(barra_x, barra_y + 2, barra_largura_padrao, barra_altura - 4)
-        energia01 = pg.Rect(barra_x, barra_y + 2, self.reverter, barra_altura - 4)
-
-        pg.draw.rect(self.game.Tela, AMARELO, barra01 )
-        pg.draw.rect(self.game.Tela, VERMELHO, energia01) 
-        # pg.draw.rect(self.game.Tela, BRANCO, barra01, 3)
-        
-        
-# #barra direita Player 02
-        # pg.draw.rect(self.game.Tela, VERMELHO, barra02)          
-        # pg.draw.rect(self.game.Tela, AMARELO, energia02) 
-        # pg.draw.rect(self.game.Tela, BRANCO, barra02, 3) 
 
 ## CRONOMETRO
+        fonte_tempo = pg.font.SysFont('arial', 53, True, False)
+
         if int(self.game.Tempo) < 30 or self.reverter > 300:
             cor_tempo = VERMELHO
                 
@@ -68,11 +69,13 @@ class Placar:
         # ADICIONAR CASA A ESQUERDA
         if int(self.game.Tempo) < 10:
             tempo = fonte_tempo.render(f'0{int(self.game.Tempo)}', True, cor_tempo)
+            tempo_sombra = fonte_tempo.render(f'0{ int(self.game.Tempo) }', True, PRETO)
 
-        barra_tempo = pg.Rect(alinhar_centro(60), branca.y + barra_altura - 3, 60, barra_altura)        
-        barra_tempo_combra = pg.Rect(alinhar_centro(60) + 2, branca.y + barra_altura, 60, barra_altura)
+        barra_tempo = pg.Rect(self.alinhar_centro(60, TELA_LARGURA ), self.barra_altura + 23, 60, self.barra_altura)        
+        barra_tempo_combra = pg.Rect(self.alinhar_centro(60, TELA_LARGURA) + 2, self.barra_altura + 26, 60, self.barra_altura)
+    
         self.game.Tela.blit(tempo_sombra, barra_tempo_combra)
         self.game.Tela.blit(tempo, barra_tempo)        
 
         if DEBUG:
-            pg.draw.line(self.game.Tela, 'White', (TELA_CENTRO_HORIZONTAL, 0), (TELA_CENTRO_HORIZONTAL, TELA_ALTURA), 3)
+            pg.draw.line(self.game.Tela, 'White', (TELA_CENTRO_V, 0), (TELA_CENTRO_V, TELA_ALTURA), 3)
